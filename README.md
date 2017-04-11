7058d82444eaa3ea8a377663a752fecc5124c042
chaokunyang <chaokunyang@yahoo.com>
2017-04-11 23:51:21 +0800

    Initial commit: 一个基于Spring Boot、Spring Batch的批处理Demo

---
+++ README.md
@@ -0,0 +1,3 @@
+# Spring Batch Demo
+
+一个基于Spring Boot、Spring Batch的批处理Demo
---
+++ src/main/java/com/timeyang/hello/Application.java
@@ -0,0 +1,15 @@
+package com.timeyang.hello;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author chaokunyang
+ */
+@SpringBootApplication
+public class Application {
+
+    public static void main(String[] args) throws Exception {
+        SpringApplication.run(Application.class, args);
+    }
+}
